073f111e724c5a863a6ab5360c0a29b82e80388d
Lucien <lucien.zak@laplateforme.io>
2022-09-22 17:02:30 +0200

    newType + commit befor eastAdmin

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Articles;
 use App\Entity\Comments;
+use App\Form\ArticleType;
 use App\Form\CommentType;
+use DateTime;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
@@ -41,17 +43,55 @@ function articles(ManagerRegistry $doctrine, Request $request): Response
 }
 
 #[Route('/article/{id}', name:'article')]
-function article(ManagerRegistry $doctrine, $id): Response
+function article(ManagerRegistry $doctrine, $id, Request $request): Response
     {
-    $comment = new Comments();
-    $comment->setUser($this->getUser());
-    $form = $this->createForm(CommentType::class, $comment);
     $entityManager = $doctrine->getManager();
     $article = $entityManager->getRepository(Articles::class)->find($id);
+    $comment = new Comments();
+    $comment->setUser($this->getUser());
+    $comment->setArticles($article);
+    $form = $this->createForm(CommentType::class, $comment);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $entityManager->persist($comment);
+        $entityManager->flush();
+    }
+
     return $this->render('blog/article.html.twig', [
         'article' => $article,
-        'commentTypeForm' => $form->createView()
+        'commentTypeForm' => $form->createView(),
     ]);
 
+}
+#[Route('/newarticle', name:'article_form')]
+function newArticle(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $article = new Articles();
+        $article->setCreatedAt(\DateTimeImmutable::createFromMutable(new DateTime()))
+        ->setUser($this->getUser());
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('Image')->getData();
+            if ($imageFile){
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = "/".$this->getParameter('image_path').$safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+                $imageFile->move(
+                    $this->getParameter('image_path'),
+                    $newFilename
+                );
+                $article->setImage($newFilename);
+            }
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            return $this->redirectToRoute('articles');
+        }
+        
+    return $this->render('blog/articleForm.html.twig', [
+        'test' => 'test',
+        'artcleFormType' => $form->createView() 
+    ]);
 }
 }
